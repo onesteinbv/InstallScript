0ed411becc80c04bd6f689b2023d25f208a45e5a
Andrea Stirpe <a.stirpe@onestein.nl>
2019-08-06 15:52:58 +0200

    Remove obsolete code

--- odoo_install.sh
+++ odoo_install.sh
@@ -44,10 +44,6 @@ WKHTMLTOX_X32=https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.
 # Update Server
 #--------------------------------------------------
 echo -e "\n---- Update Server ----"
-# universe package is for Ubuntu 18.x
-sudo add-apt-repository universe
-# libpng12-0 dependency for wkhtmltopdf
-sudo add-apt-repository "deb http://mirrors.kernel.org/ubuntu/ bionic main"
 sudo apt update -y
 sudo apt upgrade -y
 

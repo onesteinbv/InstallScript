0f0f094cb51e3b866e2b20e99f8a98cbbdfa3b8e
tarteo <d.sluijk@onestein.nl>
2020-09-08 11:40:22 +0200

    Fix install script

--- odoo_install.sh
+++ odoo_install.sh
@@ -67,7 +67,7 @@ echo -e "\n---- Install python packages ----"
 sudo apt-get install python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-decorator python-requests python-passlib python-pil -y python-suds
 
 echo -e "\n---- Install python libraries ----"
-sudo pip install gdata psycogreen ofxparse XlsxWriter xlrd pybabel pyPdf
+sudo pip install gdata psycogreen ofxparse XlsxWriter xlrd Babel pyPdf
 
 echo -e "\n--- Install other required packages"
 sudo apt-get install node-clean-css -y
